69cfd695d579c4d6bb19806aaa002c55de42838e
Andrine99 <s360219@oslomet.no>
2024-03-03 15:36:21 +0100

    Nå fungerer det.

--- src/main/resources/static/kinobilletter.js
+++ src/main/resources/static/kinobilletter.js
@@ -21,40 +21,48 @@ function kjopBillett() {
         epost: valgtEpost
     };
 
+    let alleValideringeneBestatt = true;
+
     if(billett1.antall === ""){
         document.getElementById("feilAntall").innerHTML = "Må skrive noe inn i antall";
+        alleValideringeneBestatt = false;
     }
     else{
         document.getElementById("feilAntall").innerHTML = "";
     }
     if(billett1.fornavn === ""){
         document.getElementById("feilFornavn").innerHTML = "Må skrive inn fornavn";
+        alleValideringeneBestatt = false;
     }
     else{
         document.getElementById("feilFornavn").innerHTML = "";
     }
     if(billett1.etternavn === ""){
         document.getElementById("feilEtternavn").innerHTML = "Må skrive inn etternavn";
+        alleValideringeneBestatt = false;
     }
     else {
         document.getElementById("feilEtternavn").innerHTML = "";
     }
     if(!telefonRegex.test(valgtTelefonnr)){
         document.getElementById("feilTlfnr").innerHTML = "Må skrive inn et gyldig telefonnummer";
+        alleValideringeneBestatt = false;
     }
     else {
         document.getElementById("feilTlfnr").innerHTML = "";
     }
     if(!epostRegex.test(valgtEpost)){
         document.getElementById("feilEpost").innerHTML = "Må skrive inn en gyldig e-postadresse";
+        alleValideringeneBestatt = false;
     }
     else {
         document.getElementById("feilEpost").innerHTML = "";
     }
 
-    billettene.push(billett1);
-    skrivUt();
-
+    if(alleValideringeneBestatt) {
+        billettene.push(billett1);
+        skrivUt();
+    }
     document.getElementById("filmer").value = "";
     document.getElementById("antall").value = "";
     document.getElementById("fornavn").value = "";
@@ -67,9 +75,9 @@ function kjopBillett() {
 function skrivUt() { //kjører gjennom en for-løkke for å skrive ut verdier vi har laget i objektet og satt inn i arrayet.
     let ut = "";
     for (let i = 0; i < billettene.length; i++) {
-        ut += "Kinofilm: "+billettene[i].film + "Antall billetter: " + billettene[i].antall + " .Navn: " + billettene[i].fornavn
-            + " " + billettene[i].etternavn + " .Telefonnummer: " + billettene[i].telefonnummer +
-            " .E-post: " + billettene[i].epost+ "<br>";
+        ut += "Kinofilm: "+billettene[i].film + ". Antall billetter: " + billettene[i].antall + ". Navn: " + billettene[i].fornavn
+            + billettene[i].etternavn + ". Telefonnummer: " + billettene[i].telefonnummer +
+            ". E-post: " + billettene[i].epost+ "<br>";
     }
     document.getElementById("utskrift").innerHTML = ut;
 }
